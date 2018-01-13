8fe130bf082832ee8052a3cb951d1dd0cc8aafda
Chih-Wei Tang <4719940+tangchihwei@users.noreply.github.com>
2018-01-12 22:04:03 -0800

    .

--- flask_try.py
+++ flask_try.py
@@ -49,7 +49,9 @@ def anova_task(messages):
     while True:
         for message in messages:
             print "in anova task message queue"
-            print app.messages.pop()
+            print messages.pop()
+            print "message key is: "
+            print messages["key"]
             if message["key"] is "TASK_ANOVA":
                 print "new cook order"
                 if message["key"]["event"] is "COOK_ORDER":
